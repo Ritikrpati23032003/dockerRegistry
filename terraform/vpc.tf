959d3aa62fa09fe6cac4df3a6c7ae04d3d1dcc1d
Ritik Ranjan Pati <ritikranjanpati@gmail.com>
2026-02-21 11:25:14 +0530

    Add VPC and public subnets configuration

---
+++ terraform/vpc.tf
@@ -0,0 +1,59 @@
+resource "aws_vpc" "eks_vpc" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "${var.cluster_name}-vpc"
+  }
+}
+
+resource "aws_subnet" "public_subnet_1" {
+  vpc_id                  = aws_vpc.eks_vpc.id
+  cidr_block              = var.public_subnet_1_cidr
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "public_subnet_2" {
+  vpc_id                  = aws_vpc.eks_vpc.id
+  cidr_block              = var.public_subnet_2_cidr
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = true
+}
+
+
+
+# 1️⃣ Internet Gateway
+resource "aws_internet_gateway" "eks_igw" {
+  vpc_id = aws_vpc.eks_vpc.id
+
+  tags = {
+    Name = "${var.cluster_name}-igw"
+  }
+}
+
+# 2️⃣ Route Table
+resource "aws_route_table" "eks_public_rt" {
+  vpc_id = aws_vpc.eks_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.eks_igw.id
+  }
+
+  tags = {
+    Name = "${var.cluster_name}-public-rt"
+  }
+}
+
+# 3️⃣ Route Table Associations
+resource "aws_route_table_association" "public_subnet_1_assoc" {
+  subnet_id      = aws_subnet.public_subnet_1.id
+  route_table_id = aws_route_table.eks_public_rt.id
+}
+
+resource "aws_route_table_association" "public_subnet_2_assoc" {
+  subnet_id      = aws_subnet.public_subnet_2.id
+  route_table_id = aws_route_table.eks_public_rt.id
+}
