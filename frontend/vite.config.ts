f224038d27bfb23a29c010bfee76c77778a030d8
Ritik Ranjan Pati <ritikranjanpati@gmail.com>
2026-02-21 14:53:04 +0530

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -10,7 +10,10 @@ export default defineConfig(({ mode }) => {
     server: {
       host: true,          // allow connections from outside localhost
       port: 5173,          // dev server port
-      allowedHosts: 'all', // allow all hosts (for ELB/Docker)
+       allowedHosts: [
+        'a5e3f5d76f39944d58b67e5701c2333d-179004595.us-east-1.elb.amazonaws.com',
+        'localhost',
+        ], // allow all hosts (for ELB/Docker)
       proxy: {
         '/api': {
           target: 'http://10.100.45.208:6500',
